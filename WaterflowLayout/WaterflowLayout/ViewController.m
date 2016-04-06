43bcd92f7b9521fa066f47d1f044181629a8b97e
xiaocai <tycai1@sina.com>
2016-04-06 23:43:49 +0800

    集成上下拉刷新操作

--- WaterflowLayout/WaterflowLayout/ViewController.m
+++ WaterflowLayout/WaterflowLayout/ViewController.m
@@ -11,12 +11,15 @@
 #import "CollectionViewCell.h"
 #import "Shop.h"
 #import "MJExtension.h"
+#import "MJRefresh.h"
 
 static NSString * const ID = @"shop";
 
 @interface ViewController () <UICollectionViewDataSource, UICollectionViewDelegate, WaterflowLayoutDelegate>
 
 @property (nonatomic, strong) NSMutableArray *shops;
+
+@property (nonatomic, weak) UICollectionView *collectionView;
 
 @end
 
@@ -51,6 +54,31 @@ static NSString * const ID = @"shop";
     [collection registerNib:[UINib nibWithNibName:@"CollectionViewCell" bundle:nil] forCellWithReuseIdentifier:ID];
     
     [self.view addSubview:collection];
+    self.collectionView = collection;
+    
+    //4.增加上拉刷新控件
+    [self.collectionView addFooterWithTarget:self action:@selector(loadMoreShops)];
+    
+    //4.增加下拉刷新控件
+    [self.collectionView addHeaderWithTarget:self action:@selector(headerLoadMoreShops)];
+}
+//上拉刷新
+- (void)loadMoreShops{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        NSArray *shopArray = [Shop objectArrayWithFilename:@"1.plist"];
+        [self.shops addObjectsFromArray:shopArray];
+        [self.collectionView reloadData];
+        [self.collectionView footerEndRefreshing];
+    });
+}
+//下拉刷新
+- (void)headerLoadMoreShops{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        NSArray *array = [Shop objectArrayWithFilename:@"1.plist"];
+        [self.shops addObjectsFromArray:array];
+        [self.collectionView reloadData];
+        [self.collectionView headerEndRefreshing];
+    });
 }
 
 #pragma mark - UICollectionViewDataSource
